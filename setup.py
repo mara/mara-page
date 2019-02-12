2fab562ef081dbafe0ea7a28e2f380b62537660d
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-02-12 23:17:30 +0100

    Require Python >= 3.6

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='mara-page',
     version='1.3.0',
 
+    python_requires='>=3.6',
     description='Minimal API for defining pages of Flask-based backends independently from the actual backend infrastructure',
 
     install_requires=[
